a7d8f3e84b94c2e9c59686ecee796ac1bf9f6f58
mengjin <mengjin@jecn.com.cn>
2022-02-08 18:59:51 +0800

    feat(): add something

---
+++ packages/common/src/utils/array.ts
@@ -0,0 +1,26 @@
+import {isArray,isEmpty} from 'lodash';
+
+console.log("_",isEmpty);
+
+function isUnEmptyArray(value?:any):boolean {
+ return isArray(value) && !isEmpty(value)
+}
+
+export {isUnEmptyArray}
+
+
+/**
+ * 返回重新排序后的新数组
+ * @param {Array} list 数组或者类数组结构
+ * @param {Number} startIndex 开始位置
+ * @param {Number} endIndex 结束位置
+ * @return {Array} 重新排序后新数组
+ */
+// export const reorderArray = (list:any[], startIndex:number, endIndex:number):any[] => {
+//   const result = Array.from(list);
+//   const [removed] = result.splice(startIndex, 1);
+//   result.splice(endIndex, 0, removed);
+//
+//   return result;
+// };
+
