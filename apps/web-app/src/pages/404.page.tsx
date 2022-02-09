5c49100e1ae5466a01a23fd4a63320b1cc0886dc
mengjin <mengjin@jecn.com.cn>
2022-02-09 17:39:26 +0800

    feat(): 调整web-app项目目录结构

---
+++ apps/web-app/src/pages/404.page.tsx
@@ -0,0 +1,22 @@
+import { Button, Result } from 'antd';
+import React from 'react';
+
+function backToHome() {
+    console.log("返回首页")
+}
+
+const NoFoundPage = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="抱歉，你访问的页面不存在"
+    extra={
+      <Button type="primary" onClick={backToHome}>
+        返回首页
+      </Button>
+    }
+  />
+);
+
+
+export default NoFoundPage;
