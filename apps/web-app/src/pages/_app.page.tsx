3f3891a289301565abd14a498b6d4f1db8de54c0
mengjin <mengjin@jecn.com.cn>
2022-02-10 15:24:05 +0800

    chore(配置项): 添加styled-components

--- apps/web-app/src/pages/_app.page.tsx
+++ apps/web-app/src/pages/_app.page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={{ PRIMARY_COLOR: '#4185f3' }}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
