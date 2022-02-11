bc19d9d9cc961cc711a477ec0c21753c5a9f29a5
mengjin <mengjin@jecn.com.cn>
2022-02-11 15:42:19 +0800

    feat(pro-layout): 引入@ant-design/pro-layout

--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+/* eslint-disable */
+const withPlugins = require('next-compose-plugins');
+const withAntdLess = require('next-plugin-antd-less');
+const pluginAntdLess = withAntdLess({}); // 支持antd-less
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts'],
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([[pluginAntdLess]], {
+  webpack(config) {
+    return config;
+  },
+  ...nextConfig,
+});
--- apps/web-app/src/pages/_app.page.tsx
+++ apps/web-app/src/pages/_app.page.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-
+import dynamic from 'next/dynamic';
+import 'antd/dist/antd.css';
 import '../styles/globals.css';
+
+// 必须这样导入，不然会报 "Cannot use import statement outside a module", see https://github.com/vercel/next.js/issues/32445#issuecomment-993746962
+// TODO 有更好的解决方案后续代替
+const ProLayout = dynamic(() => import('@ant-design/pro-layout'), { ssr: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={{ PRIMARY_COLOR: '#4185f3' }}>
-      <Component {...pageProps} />
+      <ProLayout
+        menuFooterRender={() => {
+          return null;
+        }}
+        menuItemRender={() => {
+          return null;
+        }}
+        rightContentRender={() => {
+          return null;
+        }}
+      >
+        <Component {...pageProps} />
+      </ProLayout>
     </ThemeProvider>
   );
 }
