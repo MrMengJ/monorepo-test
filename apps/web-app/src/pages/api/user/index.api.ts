d366ee3eb1004badee9dc798c41e7e73af141f18
mengjin <mengjin@jecn.com.cn>
2022-02-14 15:12:58 +0800

    feat(demo): 添加user.api.ts示例

--- apps/web-app/src/pages/api/user/index.api.ts
+++ apps/web-app/src/pages/api/user/index.api.ts
@@ -2,9 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
+  id: string;
   name: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' });
+export default function getUserRequest(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: 'admin', id: 'ldflsdafe' });
 }
